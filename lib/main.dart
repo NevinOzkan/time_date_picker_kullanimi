377a0eac1558455f2252b871afa7e07d827e1fbe
NevinOzkan <ozkannevin73@gmail.com>
2023-01-31 19:15:34 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var tfTarih = TextEditingController();
+  var tfSaat = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: tfSaat,
+              decoration: InputDecoration(
+                hintText: "Saat giriniz",
+              ),
+              onTap: () {
+                showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.fromDateTime(DateTime.now()),
+                ).then((alinanSaat) {
+                  setState(() {
+                    tfSaat.text = "${alinanSaat!.hour}:${alinanSaat.minute}";
+                  });
+                });
+              },
+            ),
+            TextField(
+              controller: tfTarih,
+              decoration: InputDecoration(
+                hintText: "Tarih giriniz",
+              ),
+              onTap: () {
+                showDatePicker(
+                  context: context,
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2050),
+                  initialDate: DateTime.now(),
+                ).then((alinanTarih) {
+                  setState(() {
+                    tfTarih.text =
+                        "${alinanTarih!.day}/${alinanTarih!.month}/${alinanTarih.year}";
+                  });
+                });
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
